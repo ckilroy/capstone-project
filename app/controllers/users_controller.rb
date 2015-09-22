24f2cc0b2f3252fa19001a4e957a93d87cddf861
Caitlin <caitlin.kilroy@gmail.com>
2015-09-22 10:52:53 -0400

    Creates default Team and Personal Workspaces when User is created.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,14 @@ class UsersController < ApplicationController
 
     if @user.save
       sign_in!(@user)
+      default_team_workspace = Workspace.create({name: "Team"})
+      UserWorkspace.create({user_id: @user.id, workspace_id: default_team_workspace.id})
+      default_personal_workspace = Workspace.create({name: "Personal"})
+      UserWorkspace.create({user_id: @user.id, workspace_id: default_personal_workspace.id})
       redirect_to root_url
     else
       flash.now[:errors] = @user.errors.full_messages
-      render :new
+      redirect_to root_url
     end
   end
 
