4865be17dca643163330fad091a15b4ef31dab8c
Caitlin Kilroy <caitlin.kilroy@gmail.com>
2015-10-30 03:33:52 -0400

    Create user invalid password, redirects to users/new instead of root page. Not displaying error meessages though.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
       redirect_to root_url
     else
       flash.now[:errors] = @user.errors.full_messages
-      redirect_to root_url
+      render :new
     end
   end
 
